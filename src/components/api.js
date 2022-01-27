2f5f1f14861525941d765b594833becad1cca5ba
Elmechanicus <elmechanicus@yandex.ru>
2022-01-27 20:32:43 +0300

    Создать связь с сервером

---
+++ src/components/api.js
@@ -0,0 +1,76 @@
+import {settingsAuth} from "./constants.js";
+
+export const initialServerCards = () => {//вытаскиваем карточки из сервера
+  return fetch(`${settingsAuth.apiURL}/cards`, {
+    headers: settingsAuth.headers
+  })
+    .then(res => checkResult(res));
+}
+
+export const getUser = () => {//получаем данные о профиле пользователя
+  return fetch(`${settingsAuth.apiURL}/users/me`, {
+    headers: settingsAuth.headers
+  })
+  .then(res => checkResult(res));
+}
+
+export const patchUser = (userName, aboutUser) => {//отсылаем отредактированные данные профиля пользователя
+  return fetch(`${settingsAuth.apiURL}/users/me`, {
+    method: 'PATCH',
+    headers: settingsAuth.headers,
+    body: JSON.stringify({
+      name: userName,
+      about: aboutUser
+    })
+  })
+}
+
+export const addCardToServer = (nameCard, urlCard) => {//создаём новую карточку на сервере
+  return fetch(`${settingsAuth.apiURL}/cards`, {
+    method: 'POST',
+    headers: settingsAuth.headers,
+    body: JSON.stringify({
+      name: nameCard,
+      link: urlCard,
+
+    }),
+  })
+  .then(res => checkResult(res));
+}
+
+export const deleteCardFromServer = (cardId) => {//удаляем карточку с сервера
+  return fetch(`${settingsAuth.apiURL}/cards/${cardId}`, {
+    method: 'DELETE',
+    headers: settingsAuth.headers
+  })
+    .then(res => {
+      if (!res.ok) {
+      return Promise.reject(`Ошибка: ${res.status}`)
+    }
+  })
+}
+
+export const setLikeInCard = (cardId) => {//поставим лайк карточке
+  return fetch(`${settingsAuth.apiURL}/cards/likes/${cardId}`, {
+    method: 'PUT',
+    headers: settingsAuth.headers
+  })
+  .then(res => checkResult(res));
+}
+
+export const resetLikeInCard = (cardId) => {//уберём лайк из карточки
+  return fetch(`${settingsAuth.apiURL}/cards/likes/${cardId}`, {
+    method: 'DELETE',
+    headers: settingsAuth.headers
+  })
+  .then(res => checkResult(res));
+}
+
+
+//проверим результат запроса
+function checkResult(result) {
+if (result.ok) {
+        return result.json();
+      }
+      return Promise.reject(`Ошибка: ${result.status}`);
+}
